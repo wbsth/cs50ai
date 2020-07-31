b377eef00832aa6b13b35e69b480165161a6b2dc
Michal Ungeheuer <wbsth@users.noreply.github.com>
2020-07-31 18:13:45 +0200

    Project 1, Knights - completed

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -12,21 +12,45 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Not(And(AKnight, AKnave)),  # cannot be knight and knave at the same time
+    Or(AKnight, AKnave),         # will be Knight or Knave
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Not(And(AKnight, AKnave)),  # cannot be knight and knave at the same time
+    Or(AKnight, AKnave),        # will be Knight or Knave
+
+    Not(And(BKnight, BKnave)),  # cannot be knight and knave at the same time
+    Or(BKnight, BKnave),        # will be Knight or Knave
+
+    # Or(And(AKnight, BKnave), And(AKnave, BKnight)), # A and B can't be the same figure
+
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Not(And(AKnight, AKnave)),  # cannot be knight and knave at the same time
+    Or(AKnight, AKnave),        # will be Knight or Knave
+
+    Not(And(BKnight, BKnave)),  # cannot be knight and knave at the same time
+    Or(BKnight, BKnave),        # will be Knight or Knave
+
+    # Or(And(AKnight, BKnave), And(AKnave, BKnight)), # A and B can't be the same figure
+
+    Implication(AKnight, And(AKnight, BKnight)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+
+    Implication(BKnight, And(BKnight, AKnave)),
+    Implication(BKnave, Not(And(BKnave, AKnight)))
 )
 
 # Puzzle 3
@@ -35,7 +59,63 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Not(And(AKnight, AKnave)),  # A cannot be knight and knave at the same time
+    Or(AKnight, AKnave),        # A will be Knight or Knave
+
+    Not(And(BKnight, BKnave)),  # B cannot be knight and knave at the same time
+    Or(BKnight, BKnave),        # B will be Knight or Knave
+
+    Not(And(CKnight, CKnave)),  # C cannot be knight and knave at the same time
+    Or(CKnight, CKnave),        # C will be Knight or Knave
+
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Or(
+        # "I am a knight."
+        And(
+            Implication(AKnight, AKnight),
+            Implication(AKnave, Not(AKnight))
+        ),
+        
+        # "I am a knave."
+        And(
+            Implication(AKnight, AKnave),
+            Implication(AKnave, Not(AKnave))
+        )
+    ),
+
+    Not(And(
+        # "I am a knight."
+        And(
+            Implication(AKnight, AKnight),
+            Implication(AKnave, Not(AKnight))
+        ),
+        
+        # "I am a knave."
+        And(
+            Implication(AKnight, AKnave),
+            Implication(AKnave, Not(AKnave))
+        )
+    )),
+
+    # B says "A said 'I am a knave'."
+    Implication(BKnight, And(
+        Implication(AKnight, AKnave),
+        Implication(AKnave, Not(AKnave))
+    )),
+
+    Implication(BKnave, Not(And(
+        Implication(AKnight, AKnave),
+        Implication(AKnave, Not(AKnave))
+    ))),
+
+
+    # B says "C is a knave."
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    # C says "A is a knight."
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
 )
 
 
